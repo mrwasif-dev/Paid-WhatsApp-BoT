b83a232ff2537c4dca56711aa9a891c689d9cbe8
mrwasif-dev <happyprincetemp@gmail.com>
2026-01-30 23:42:50 +0500

    Delete wasiplugins directory

--- wasiplugins/jid.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'jid',
-    category: 'Debug',
-    desc: 'Get the JID of the current chat',
-    wasi_handler: async (wasi_sock, wasi_sender) => {
-        await wasi_sock.sendMessage(wasi_sender, { text: `🆔 *JID:* ${wasi_sender}` });
-    }
-};
