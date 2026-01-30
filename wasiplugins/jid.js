c237c9fcaf0ea5abdf12ce67da3ee95865cbde28
mrwasif-dev <happyprincetemp@gmail.com>
2026-01-31 00:11:30 +0500

    Update jid.js

--- wasiplugins/jid.js
+++ wasiplugins/jid.js
@@ -3,6 +3,6 @@ module.exports = {
     category: 'Debug',
     desc: 'Get the JID of the current chat',
     wasi_handler: async (wasi_sock, wasi_sender) => {
-        await wasi_sock.sendMessage(wasi_sender, { text: `🆔 *JID:* ${wasi_sender}` });
+        await wasi_sock.sendMessage(wasi_sender, { text: `${wasi_sender}` });
     }
 };
