620b5b1e9a41b791e64c83bc703696e96958b43f
mrwasif-dev <happyprincetemp@gmail.com>
2026-02-10 08:43:30 +0500

    Delete wasi.js

--- wasi.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    sessionId: process.env.SESSION_ID || '',
-    mongoDbUrl: process.env.MONGO_URL || '',
-    ownerNumber: process.env.OWNER_NUMBER || '923039107958@s.whatsapp.net',
-    prefix: process.env.PREFIX || '!'
-};
-
-module.exports = { config };
